166d37254ab9421a4110bd5ca4db666b424eae0d
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2023-11-10 16:12:40 +0900

    [feat] radar chart에서 특정 영역 선택하면 위치와 value 띄움
    - 위치와 값을 알았기 때문에, 각 영역을 자세히 보여주는 그림을 아래에 띄우고 (image, comment) 형식으로 지도가 필요하다고 알려주도록 수정
    
    Todo
    - fever time을 어디에 띄워야 할까? 우선 chart가 그날의 양치에 관한 기록이니까 인스타 스토리처럼 아이의 프로필 사진과 함께 클릭하면 fever time이 나오도록 할 것
    - 그 옆에 방금 한 최근의 양치에 대한 점수? or 시간?

--- app/src/main/java/com/lite/holistic_tracking/ParentMenuChartFragment.java
+++ app/src/main/java/com/lite/holistic_tracking/ParentMenuChartFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,14 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.lite.holistic_tracking.Database.ToothbrushingDB;
 import com.lite.holistic_tracking.Entity.Toothbrushing;
 
@@ -76,41 +81,45 @@ public class ParentMenuChartFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_parent_menu_chart, container, false);
         RadarChart radarChart = view.findViewById(R.id.mapsearchdetail_radar_chart);
+
+        // 데이터베이스에서 정보를 가져오는 작업을 백그라운드 스레드에서 실행
         new Thread(new Runnable() {
             @Override
             public void run() {
                 // 백그라운드 스레드에서 데이터베이스에서 정보를 가져옴
                 List<Toothbrushing> toothbrushingList = ToothbrushingDB.getDatabase(getContext()).toothbrushingDao().getAll();
+                // 데이터가 존재하는 경우
+                if (toothbrushingList.size() != 0) {
+                    Toothbrushing toothbrushing = toothbrushingList.get(1); // 첫 번째 Toothbrushing 객체를 가져옴
+                    ArrayList<RadarEntry> dataVals = new ArrayList<>();
+                    dataVals.add(new RadarEntry(toothbrushing.getLeft_circular()));
+                    dataVals.add(new RadarEntry(toothbrushing.getMid_circular()));
+                    dataVals.add(new RadarEntry(toothbrushing.getRight_circular()));
+                    dataVals.add(new RadarEntry(toothbrushing.getLeft_upper()));
+                    dataVals.add(new RadarEntry(toothbrushing.getLeft_lower()));
+                    dataVals.add(new RadarEntry(toothbrushing.getRight_upper()));
+                    dataVals.add(new RadarEntry(toothbrushing.getRight_lower()));
+                    dataVals.add(new RadarEntry(toothbrushing.getMid_vertical_upper()));
+                    dataVals.add(new RadarEntry(toothbrushing.getMid_vertical_lower()));
 
-                // UI 업데이트를 메인 스레드에서 처리
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // 데이터가 존재하는 경우
-                        if (toothbrushingList.size() != 0) {
-                            Toothbrushing toothbrushing = toothbrushingList.get(1); // 첫 번째 Toothbrushing 객체를 가져옴
-                            ArrayList<RadarEntry> dataVals = new ArrayList<>();
-                            dataVals.add(new RadarEntry(toothbrushing.getLeft_circular()));
-                            dataVals.add(new RadarEntry(toothbrushing.getMid_circular()));
-                            dataVals.add(new RadarEntry(toothbrushing.getRight_circular()));
-                            dataVals.add(new RadarEntry(toothbrushing.getLeft_upper()));
-                            dataVals.add(new RadarEntry(toothbrushing.getLeft_lower()));
-                            dataVals.add(new RadarEntry(toothbrushing.getRight_upper()));
-                            dataVals.add(new RadarEntry(toothbrushing.getRight_lower()));
-                            dataVals.add(new RadarEntry(toothbrushing.getMid_vertical_upper()));
-                            dataVals.add(new RadarEntry(toothbrushing.getMid_vertical_lower()));
-
+                    // UI 업데이트를 메인 스레드에서 처리
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
                             RadarDataSet radarDataSet = new RadarDataSet(dataVals, "toothbrushing");
                             radarDataSet.setColor(Color.RED);
                             radarDataSet.setLineWidth(2f);
                             radarDataSet.setValueTextColor(Color.RED);
                             radarDataSet.setValueTextSize(14f);
 
+                            // 값(데이터)을 표시하지 않도록 설정
+                            radarDataSet.setDrawValues(false);
+
                             RadarData radarData = new RadarData();
                             radarData.addDataSet(radarDataSet);
 
                             String[] labels = {"left_circular", "mid_circular", "right_circular", "left_upper"
-                            , "left_upper", "left_lower", "right_upper", "right_lower"
+                                    , "left_upper", "left_lower", "right_upper", "right_lower"
                                     , "mid_vertical_upper", "mid_vertical_lower"};
 
                             XAxis xAxis = radarChart.getXAxis();
@@ -118,11 +127,42 @@ public class ParentMenuChartFragment extends Fragment {
 
                             radarChart.setData(radarData);
 
-                        } else {
+                            // 중앙값을 0으로 설정
+                            radarChart.getYAxis().setAxisMinimum(0);
 
+                            // 차트가 화면에 나타날 수 있도록 설정
+                            radarChart.setVisibility(View.VISIBLE);
+                            radarChart.invalidate(); // 차트 갱신
+
+                            // RadarChart에 이벤트 리스너 추가
+                            radarChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+                                @Override
+                                public void onValueSelected(Entry e, Highlight h) {
+                                    // 선택한 값(e)을 이용하여 원하는 동작 수행
+                                    // 이 부분에서 선택한 값(e)을 가지고 아래에 값을 표시하는 등의 동작을 수행할 수 있습니다.
+                                    int index = (int) h.getX(); // 선택한 피자 조각(영역)의 인덱스
+
+                                    // 해당 인덱스에 대한 라벨 가져오기
+
+                                    //IRadarDataSet dataSet = radarChart.getData().getDataSetByIndex(index); // 여기서 0은 dataSet의 index
+                                    String label = labels[index];
+                                    int value = (int) e.getY();
+
+                                    Log.v("test", "Selected: " + label + ", Value: " + value);
+
+                                }
+
+                                @Override
+                                public void onNothingSelected() {
+                                    // 아무 값도 선택되지 않았을 때의 동작 정의
+                                }
+                            });
                         }
-                    }
-                });
+                    });
+                } else {
+                    // 데이터가 없을 때의 처리
+                }
+
             }
         }).start();
         return view;
