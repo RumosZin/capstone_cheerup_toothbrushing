6e5f3e93cfa66b7bf4a0d576b6cbb48665536a2e
gwaksital <80732503+gwaksital@users.noreply.github.com>
2023-11-15 19:37:22 +0900

    0

--- app/src/main/java/com/lite/holistic_tracking/HeeJunActivity.java
+++ app/src/main/java/com/lite/holistic_tracking/HeeJunActivity.java
@@ -12,7 +12,9 @@ public class HeeJunActivity extends AppCompatActivity {
     public ImageView movingImage;
     public Handler handler;
     public float angle = 0;
-    public final float radius = 200;
+    public final float radius = 5;
+    private float initialX;
+    private float initialY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,9 @@ public class HeeJunActivity extends AppCompatActivity {
 
         movingImage = findViewById(R.id.ballImage);
         handler = new Handler(Looper.getMainLooper());
+
+        initialX = movingImage.getX();
+        initialY = movingImage.getY();
 
         // 주기적으로 이미지 위치 업데이트
         startMovingAnimation();
@@ -31,7 +36,6 @@ public class HeeJunActivity extends AppCompatActivity {
             @Override
             public void run() {
                 updateImagePosition();
-                rotateImage();
                 handler.postDelayed(this, 16); // 60fps 기준으로 업데이트 (1000ms / 60)
             }
         });
@@ -42,10 +46,8 @@ public class HeeJunActivity extends AppCompatActivity {
         float cy = movingImage.getHeight() / 2.0f + movingImage.getY();
 
         // 각도를 증가시켜서 원을 따라 움직이도록 함
-        angle += 1.0f;
-        if (angle > 360) {
-            angle = 0;
-        }
+        angle += 5.0f;
+        if (angle > 360) resetAnimation();
 
         // 좌표 업데이트
         float x = (float) (cx + (radius * Math.sin(Math.toRadians(angle))));
@@ -56,13 +58,10 @@ public class HeeJunActivity extends AppCompatActivity {
         movingImage.setY(y - movingImage.getHeight() / 2.0f);
     }
 
-    private void rotateImage() {
-        // 이미지 회전 애니메이션
-        RotateAnimation rotate = new RotateAnimation(angle, angle + 1.0f,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        rotate.setDuration(16); // 60fps 기준으로 업데이트 (1000ms / 60)
-        rotate.setFillAfter(true);
-        movingImage.startAnimation(rotate);
+    private void resetAnimation() {
+        angle = 0;
+        movingImage.setX(initialX);
+        movingImage.setY(initialY);
     }
+
 }
