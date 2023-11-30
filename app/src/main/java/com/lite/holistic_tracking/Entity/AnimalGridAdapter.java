14b7df96b8f13af53de36469094e4cba8a42a469
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2023-11-30 20:13:36 +0900

    [fix] 동물 선택하면 화면 넘어가도록.

--- app/src/main/java/com/lite/holistic_tracking/Entity/AnimalGridAdapter.java
+++ app/src/main/java/com/lite/holistic_tracking/Entity/AnimalGridAdapter.java
@@ -1,6 +1,10 @@
 package com.lite.holistic_tracking.Entity;
 
+import static androidx.core.content.ContextCompat.startActivity;
+import static java.security.AccessController.getContext;
+
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +14,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.lite.holistic_tracking.BuyingDialog;
+import com.lite.holistic_tracking.HolisticActivity;
 import com.lite.holistic_tracking.R;
 
 import java.util.List;
@@ -66,11 +67,12 @@ public class AnimalGridAdapter extends BaseAdapter {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 클릭한 동물 정보를 넘김!
-//                BuyingDialog buyingDialog = new BuyingDialog(view.getContext(), child, animal);
-//                buyingDialog.show();
+                Intent intent = new Intent(mContext, HolisticActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
             }
         });
+
 
         return view;
     }
@@ -86,57 +88,4 @@ public class AnimalGridAdapter extends BaseAdapter {
             seedTextView = itemView.findViewById(R.id.textViewRequiredSeed);
         }
     }
-
-//    @NonNull
-//    @Override
-//    public AnimalGridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.animal_item, parent, false);
-//        AnimalGridAdapter.AnimalGridViewHolder vh = new AnimalGridAdapter.AnimalGridViewHolder(view);
-//
-//        return vh;
-//    }
-//
-
-
-//    @Override
-//    public void onBindViewHolder(@NonNull AnimalGridViewHolder holder, int position) {
-//        Animal animal = animalList.get(position);
-//        holder.imageView.setImageResource(animal.getImageResource());
-//        holder.nameTextView.setText(animal.getName());
-//        holder.seedTextView.setText(String.valueOf(animal.getRequiredSeed()));
-//        Log.v("ddddddddddddddddddddddddddddddddddd", animal.getName());
-//
-//        // 클릭 이벤트 리스너 추가
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // 클릭한 동물 정보를 BuyingDialog 액티비티로 전달하여 시작
-////                Intent intent = new Intent(mContext, BuyingDialog.class);
-////                intent.putExtra("animalName", animal.getName());
-////                intent.putExtra("animalDescription", animal.getRequiredSeed());
-////                mContext.startActivity(intent);
-////                BuyingDialog buyingDialog = new BuyingDialog(view.getContext(), child, animal);
-////                buyingDialog.show();
-//            }
-//        });
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return animalList.size();
-//    }
-//
-//    public static class AnimalGridViewHolder extends RecyclerView.ViewHolder {
-//        ImageView imageView;
-//        TextView nameTextView;
-//        TextView seedTextView;
-//
-//        public AnimalGridViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            imageView = itemView.findViewById(R.id.imageViewAnimal);
-//            nameTextView = itemView.findViewById(R.id.textViewAnimalName);
-//            seedTextView = itemView.findViewById(R.id.textViewRequiredSeed);
-//        }
-//    }
 }
